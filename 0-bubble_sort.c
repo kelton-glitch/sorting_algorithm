c73ce7086acb7f30492bfe87c2614c084b3d12c3
Kamgo N Elton <kamgoelton89@gmail.com>
2023-01-23 20:48:08 +0100

    fix

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,9 +1,9 @@
 #include "sort.h"
 
 /**
- * swap_ints - Swap two integers in an array.
- * @a: The first integer to swap.
- * @b: The second integer to swap.
+ * swap_ints - Swap two integers in an array
+ * @a: The first integer to swap
+ * @b: The second integer to swap
  */
 void swap_ints(int *a, int *b)
 {
@@ -16,8 +16,8 @@ void swap_ints(int *a, int *b)
 
 /**
  * bubble_sort - Sort an array of integers in ascending order.
- * @array: An array of integers to sort.
- * @size: The size of the array.
+ * @array: An array of integers to sort
+ * @size: The size of the array
  *
  * Description: Prints the array after each swap.
  */
